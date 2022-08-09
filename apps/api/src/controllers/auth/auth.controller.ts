6ce89b663611c066bd57e11e6e549c2035d45efb
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2022-08-09 22:05:01 +0200

    feat: added register endpoint

--- apps/api/src/controllers/auth/auth.controller.ts
+++ apps/api/src/controllers/auth/auth.controller.ts
@@ -1,10 +1,17 @@
-import { Get, JsonController } from "routing-controllers";
+import { JsonController, Post } from "routing-controllers";
 import { AuthHandler } from "./auth.handler";
+import { Body } from "@panenco/papi";
+import { LoginBody, UserBody } from "@battleships/contracts/src";
 
 @JsonController("/authentication")
 export class AuthController {
-  @Get("/login")
-  async login() {
-    return AuthHandler.login();
-  }
+	@Post("/login")
+	async login(@Body() body: LoginBody) {
+		return AuthHandler.login(body);
+	}
+
+	@Post("/register")
+	async register(@Body() body: UserBody) {
+		return AuthHandler.register(body);
+	}
 }
--- apps/api/src/controllers/auth/auth.handler.ts
+++ apps/api/src/controllers/auth/auth.handler.ts
@@ -1,7 +1,22 @@
+import { LoginBody, UserBody } from "@battleships/contracts";
+import { getEM } from "utils";
+import { User } from "entities";
+
 export class AuthHandler {
-  static async login() {
-    return {
-      token: "token",
-    };
-  }
+	static async login(body: LoginBody) {}
+
+	static async register(body: UserBody) {
+		const em = getEM();
+
+		if (await em.findOne(User, { email: body.email })) {
+			throw new Error("Email already exists");
+		}
+
+		const userInput = { ...body };
+		const user = em.create(User, userInput);
+		if (user.password) await user.setPassword(user.password);
+
+		await em.persistAndFlush(user);
+		return user;
+	}
 }

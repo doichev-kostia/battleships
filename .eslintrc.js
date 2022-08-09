10303dbc58704bbb95f32e3757e8f9eb42ee3a88
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2022-08-09 22:02:02 +0200

    feat: config updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,28 @@
-module.exports = {};
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+	},
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint", "prettier"],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"prettier",
+		"plugin:prettier/recommended",
+	],
+	rules: {
+		"prettier/prettier": [
+			"error",
+			{
+				useTabs: true,
+				semi: true,
+			},
+		],
+		"@typescript-eslint/no-var-requires": 0,
+		"@typescript-eslint/ban-types": "off",
+		"@typescript-eslint/no-inferrable-types": "warn",
+		"@typescript-eslint/no-explicit-any": "off",
+	},
+};

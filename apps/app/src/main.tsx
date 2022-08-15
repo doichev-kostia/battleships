ba7d166117b4af3d54a0f1b4c49b2d5cfc99cd48
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2022-08-15 17:32:52 +0200

    feat: updated app configs and providers

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -1,26 +1,35 @@
-import React, { Suspense } from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import "./reset.css";
 import "./index.css";
+
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import Router from "app/routes/router";
 import { StyledEngineProvider, ThemeProvider } from "@mui/material";
 import { theme } from "ui";
 import { Loader } from "app/components/loader";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const rootElement = document.getElementById("root") || document.body;
+// Create a client
+const queryClient = new QueryClient();
 
 const Main = () => {
 	return (
-		<BrowserRouter>
-			<ThemeProvider theme={theme}>
-				<StyledEngineProvider injectFirst>
-					<Suspense fallback={<Loader />}>
-						<Router />
-					</Suspense>
-				</StyledEngineProvider>
-			</ThemeProvider>
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
+					<StyledEngineProvider injectFirst>
+						<Suspense fallback={<Loader />}>
+							<Router />
+						</Suspense>
+					</StyledEngineProvider>
+				</ThemeProvider>
+			</BrowserRouter>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	);
 };
 
-ReactDOM.createRoot(rootElement).render(<Main />);
+ReactDOM.render(<Main />, rootElement);
